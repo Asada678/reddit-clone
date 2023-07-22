92394c28c69cedc07399cfc4d39e031e58578d6f
Asada678 <asada.tomoya0923@gmail.com>
2023-07-22 11:55:13 +0900

    (api, page)create comment, vote to comment

---
+++ src/components/CommentVotes.tsx
@@ -0,0 +1,114 @@
+"use client";
+import { useCustomToast } from "@/hooks/use-custom-toast";
+import { usePrevious } from "@mantine/hooks";
+import { CommentVote, VoteType } from "@prisma/client";
+import { useState, type FC, useEffect } from "react";
+import { ArrowBigDown, ArrowBigUp } from "lucide-react";
+import { cn } from "@/lib/utils";
+import { useMutation } from "@tanstack/react-query";
+import { CommentVotePayload, PostVotePayload } from "@/lib/validators/vote";
+import axios, { AxiosError } from "axios";
+import { toast } from "@/hooks/use-toast";
+import { Button } from "./ui/Button";
+
+type PartialVote = Pick<CommentVote, "type">;
+
+interface CommentVoteProps {
+  commentId: string;
+  initialVotesAmount: number;
+  initialVote?: PartialVote;
+}
+
+const CommentVote: FC<CommentVoteProps> = ({
+  commentId,
+  initialVotesAmount,
+  initialVote,
+}) => {
+  const { loginToast } = useCustomToast();
+  const [votesAmount, setVotesAmount] = useState<number>(initialVotesAmount);
+  const [currentVote, setCurrentVote] = useState(initialVote);
+  const prevVote = usePrevious(currentVote);
+
+  useEffect(() => {
+    setCurrentVote(initialVote);
+  }, [initialVote]);
+
+  const { mutate: vote } = useMutation({
+    mutationFn: async (voteType: VoteType) => {
+      const payload: CommentVotePayload = {
+        commentId,
+        voteType,
+      };
+
+      await axios.patch("/api/subreddit/post/comment/vote", payload);
+    },
+    onError: (error, voteType) => {
+      if (voteType === "UP") setVotesAmount((prev) => prev - 1);
+      else setVotesAmount((prev) => prev + 1);
+
+      // reset current vote
+      setCurrentVote(prevVote);
+
+      if (error instanceof AxiosError) {
+        if (error.response?.status === 401) {
+          return loginToast();
+        }
+      }
+
+      return toast({
+        title: "Something went wrong.",
+        description: "Your vote was not registered, please try again.",
+        variant: "destructive",
+      });
+    },
+    onMutate: (type: VoteType) => {
+      if (currentVote?.type === type) {
+        setCurrentVote(undefined);
+        if (type === "UP") setVotesAmount((prev) => prev - 1);
+        else if (type === "DOWN") setVotesAmount((prev) => prev + 1);
+      } else {
+        setCurrentVote({ type });
+        if (type === "UP")
+          setVotesAmount((prev) => prev + (currentVote ? 2 : 1));
+        else if (type === "DOWN")
+          setVotesAmount((prev) => prev - (currentVote ? 2 : 1));
+      }
+    },
+  });
+
+  return (
+    <div className="flex gap-1">
+      <Button
+        onClick={() => vote("UP")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
+        <ArrowBigUp
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-emerald-500 fill-emerald-500": currentVote?.type === "UP",
+          })}
+        />
+      </Button>
+
+      <p className="text-center py-2 font-medium text-sm text-zinc-900">
+        {votesAmount}
+      </p>
+
+      <Button
+        onClick={() => vote("DOWN")}
+        size="sm"
+        variant="ghost"
+        aria-label="upvote"
+      >
+        <ArrowBigDown
+          className={cn("h-5 w-5 text-zinc-700", {
+            "text-red-500 fill-red-500": currentVote?.type === "DOWN",
+          })}
+        />
+      </Button>
+    </div>
+  );
+};
+
+export default CommentVote;
